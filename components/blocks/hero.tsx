4641b8244e1a1869f00778d12d886c8f17933e32
Adam Bardsley <adam.bardsley@gmail.com>
2025-02-06 00:21:10 +0000

    Hero updates

--- components/blocks/hero.tsx
+++ components/blocks/hero.tsx
@@ -38,7 +38,7 @@ export const Hero = ({ data }: { data: PageBlocksHero }) => {
             className="relative flex-shrink-0 md:w-2/5 flex justify-center order-first md:hidden md:order-last"
           >
             <Image
-              className={`w-full h-auto max-w-full rounded-lg mb-3 ${/.*\.svg/.test(data.image.src) ? "p-12 sm:p-24 md:p-0" : "nothing"}`}
+              className={`w-full h-auto max-w-[300px] md:max-w-[400px] rounded-lg mb-3 ${/.*\.svg/.test(data.image.src) ? "p-12 sm:p-24 md:p-0" : "nothing"}`}
               style={{ objectFit: "cover" }}
               alt={data.image.alt}
               src={data.image.src}
